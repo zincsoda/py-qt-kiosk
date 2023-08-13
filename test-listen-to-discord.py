88f2240ada6b3c34984a22cadd058aaabd12951e
Steve Walsh <stevewalsh@smartretail.co>
2023-08-14 07:05:15 +0800

    no need for venv folder

---
+++ test-listen-to-discord.py
@@ -0,0 +1,27 @@
+import discord
+import discord_config
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    
+@client.event
+async def on_message(message):
+    my_dev_channel = client.get_channel(972378842494824488)
+    if message.author == client.user:
+        return
+
+    if message.channel != my_dev_channel:
+        return
+
+    print('Message from {0.author}: {0.content}'.format(message))
+    f = open("message.txt", "w")
+    f.write(message.content.strip())
+    f.close()
+
+client.run(discord_config.house_bot_token)
